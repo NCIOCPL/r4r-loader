65d01b84d26795dd6e696323d596644e08078725
Blair Learn <blairlearn@users.noreply.github.com>
2022-09-22 15:50:54 -0400

    sigh

--- integration-tests/bin/karate-config.js
+++ integration-tests/bin/karate-config.js
@@ -3,7 +3,7 @@ function fn() {
         esHost: 'http://localhost:9200'
     };
     if (java.lang.System.getenv('KARATE_ESHOST')) {
-        config.apiHost = java.lang.System.getenv('KARATE_ESHOST');
+        config.esHost = java.lang.System.getenv('KARATE_ESHOST');
     }
     return config;
 }
